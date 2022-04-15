e942a2575d56d1c290bcd984a960797ce81e3902
OLDMONEY0 <98642671+OLDMONEY0@users.noreply.github.com>
2022-04-15 15:14:01 +0600

    Add files via upload

---
+++ lab9/paint2.py
@@ -0,0 +1,207 @@
+import pygame
+
+
+WIDTH, HEIGHT = 1200, 800   # базовый размер окна
+FPS = 60
+draw = False                # нажатие, зажатие - рисуем, отжали - не рисуем 
+lastPos = (0, 0)            # базовая позиция 
+radius = 8            # базовый радиус для инструментов
+color = 'blue'              # базовый цвет
+mode = 'pen'                # базовый режим
+
+pygame.init()
+screen = pygame.display.set_mode([WIDTH, HEIGHT])
+pygame.display.set_caption('Paint')
+clock = pygame.time.Clock()
+screen.fill(pygame.Color('white')) # закрашиваем в белый цвет, чтобы внутри цикла не обновлялось
+fontRadius = pygame.font.SysFont('Arial', 66, bold=True)
+
+
+def drawLine(screen, start, end, width, color):
+    x1 = start[0]
+    x2 = end[0]
+    y1 = start[1]
+    y2 = end[1]
+    
+    dx = abs(x1 - x2)
+    dy = abs(y1 - y2)
+
+    A = y2 - y1
+    B = x1 - x2
+    C = x2 * y1 - x1 * y2
+
+    if dx > dy:
+        if x1 > x2:
+            x1, x2 = x2, x1
+            y1, y2 = y2, y1
+        for x in range(x1, x2):
+            y = (-C - A * x) / B
+            pygame.draw.circle(screen, pygame.Color(color), (x, y), width)
+    else:
+        if y1 > y2:
+            x1, x2 = x2, x1
+            y1, y2 = y2, y1
+        for y in range(y1, y2):
+            x = (-C - B * y) / A
+            pygame.draw.circle(screen, pygame.Color(color), (x, y), width)
+
+
+def drawCircle(screen, start, end, width, color):
+    x1 = start[0]
+    x2 = end[0]
+    y1 = start[1]
+    y2 = end[1]
+    x = (x1 + x2) / 2
+    y = (y1 + y2) / 2
+    radius = abs(x1 - x2) / 2
+    pygame.draw.circle(screen, pygame.Color(color), (x, y), radius, width)
+
+
+def drawRectangle(screen, start, end, width, color):
+    x1 = start[0]
+    x2 = end[0]
+    y1 = start[1]
+    y2 = end[1]
+    widthr = abs(x1 - x2)
+    height = abs(y1 - y2)
+    if x2 > x1 and y2 > y1:
+        pygame.draw.rect(screen, pygame.Color(color), (x1, y1, widthr, height), width)
+    if y2 > y1 and x1 > x2:
+        pygame.draw.rect(screen, pygame.Color(color), (x2, y1, widthr, height), width)
+    if x1 > x2 and y1 > y2:
+        pygame.draw.rect(screen, pygame.Color(color), (x2, y2, widthr, height), width)
+    if x2 > x1 and y1 > y2:
+        pygame.draw.rect(screen, pygame.Color(color), (x1, y2, widthr, height), width)
+    
+
+
+def drawSquare(screen, start, end, width, color):
+    x1 = start[0]
+    x2 = end[0]
+    y1 = start[1]
+    y2 = end[1]
+    mn = min(abs(x2 - x1), abs(y2 - y1))
+
+
+    if x2 > x1 and y2 > y1:
+        pygame.draw.rect(screen, pygame.Color(color), (x1, y1, mn, mn))
+    if y2 > y1 and x1 > x2:
+        pygame.draw.rect(screen, pygame.Color(color), (x2, y1, mn, mn))
+    if x1 > x2 and y1 > y2:
+        pygame.draw.rect(screen, pygame.Color(color), (x2, y2, mn, mn))
+    if x2 > x1 and y1 > y2:
+        pygame.draw.rect(screen, pygame.Color(color), (x1, y2, mn, mn))
+
+def drawRightTriangle(screen, start, end, width, color):
+    x1 = start[0]
+    x2 = end[0]
+    y1 = start[1]
+    y2 = end[1]
+    
+    if x2 > x1 and y2 > y1:
+        pygame.draw.polygon(screen, pygame.Color(color), ((x1, y1), (x2, y2), (x1, y2)))
+    if y2 > y1 and x1 > x2:
+        pygame.draw.polygon(screen, pygame.Color(color), ((x1, y1), (x2, y2), (x1, y2)))
+    if x1 > x2 and y1 > y2:
+        pygame.draw.polygon(screen, pygame.Color(color), ((x1, y1), (x2, y2), (x2, y1)))
+    if x2 > x1 and y1 > y2:
+        pygame.draw.polygon(screen, pygame.Color(color), ((x1, y1), (x2, y2), (x2, y1)))
+
+
+def drawEquilateralTriangle(screen, start, end, width, color):
+    x1 = start[0]
+    x2 = end[0]
+    y1 = start[1]
+    y2 = end[1]
+
+    width_b = abs(x2 - x1)
+    height = (3**0.5) * width_b / 2
+
+    if y2 > y1:
+        pygame.draw.polygon(screen, pygame.Color(color), ((x1, y2), (x2, y2), ((x1 + x2) / 2, y2 - height)), width)
+    else:
+        pygame.draw.polygon(screen, pygame.Color(color), ((x1, y1), (x2, y1), ((x1 + x2) / 2, y1 - height)))
+    
+
+def drawRhombus(screen, start, end, width, color):
+    x1 = start[0]
+    x2 = end[0]
+    y1 = start[1]
+    y2 = end[1]
+    pygame.draw.lines(screen, pygame.Color(color), True, (((x1 + x2) / 2, y1), (x1, (y1 + y2) / 2), ((x1 + x2) / 2, y2), (x2, (y1 + y2) / 2)), width)
+
+
+while True:
+    # events
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            exit()
+        
+        # Нажатия на клавиатуру
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_r:
+                mode = 'rectangle'
+            if event.key == pygame.K_c:
+                mode = 'circle'
+            if event.key == pygame.K_p:
+                mode = 'pen'
+            if event.key == pygame.K_e:
+                mode = 'erase'
+            if event.key == pygame.K_s:
+                mode = 'square'
+            if event.key == pygame.K_q:
+                screen.fill(pygame.Color('white'))
+            if event.key == pygame.K_y:
+                color = 'yellow'
+            if event.key == pygame.K_b:
+                color = 'black'
+            if event.key == pygame.K_t:
+                mode = 'right_tri'
+            if event.key == pygame.K_u:
+                mode = 'eq_tri'
+            if event.key == pygame.K_h:
+                mode = 'rhombus'
+            if event.key == pygame.K_UP:
+                radius = min(200, radius + 1)   # ограничение по максимальному размеру радиуса
+            if event.key == pygame.K_DOWN:
+                radius = max(1, radius - 1)     # ограничение по минимальному размеру радиуса
+
+        # Нажатие на мышку
+        if event.type == pygame.MOUSEBUTTONDOWN: 
+            draw = True
+            if mode == 'pen':
+                pygame.draw.circle(screen, pygame.Color(color), event.pos, radius)
+            prevPos = event.pos
+
+        # Отпускание мышки
+        if event.type == pygame.MOUSEBUTTONUP: 
+            if mode == 'rectangle':
+                drawRectangle(screen, prevPos, event.pos, radius, color)
+            elif mode == 'circle':
+                drawCircle(screen, prevPos, event.pos, radius, color)
+            elif mode == 'square':
+                drawSquare(screen, prevPos, event.pos, radius, color)
+            elif mode == 'right_tri':
+                drawRightTriangle(screen, prevPos, event.pos, radius, color)
+            elif mode == 'eq_tri':
+                drawEquilateralTriangle(screen, prevPos, event.pos, radius, color)
+            elif mode == 'rhombus':
+                drawRhombus(screen, prevPos, event.pos, radius, color)
+            draw = False
+
+        # Перемещение мышки
+        if event.type == pygame.MOUSEMOTION: 
+            if draw and mode == 'pen':
+                drawLine(screen, lastPos, event.pos, radius, color)
+            elif draw and mode == 'erase':
+                drawLine(screen, lastPos, event.pos, radius, 'white')
+            lastPos = event.pos
+
+    # show radius & color
+    pygame.draw.rect(screen, pygame.Color('white'), (5, 5, 115, 75))
+    renderRadius = fontRadius.render(f'{radius}', True, pygame.Color(color))
+    screen.blit(renderRadius, (5, 5))
+
+    # display
+    pygame.display.flip()
+    clock.tick(FPS)
---
+++ lab9/racer2.py
@@ -0,0 +1,134 @@
+from gc import collect
+import pygame 
+import sys
+import random
+import time
+
+pygame.init()
+
+FPS = 60
+SCREEN_WIDTH = 400
+SCREEN_HEIGHT = 600
+SPEED = 5
+COIN_NUM = 0
+N = 3
+COIN_REMEMBER = 0
+
+font = pygame.font.SysFont("Verdana", 60)
+font_small = pygame.font.SysFont("Verdana", 20)
+game_over = font.render("Game over", True, (255, 255, 255))
+background = pygame.image.load("images\\AnimatedStreet.png")
+SURF = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))    
+SURF.fill((255, 255, 255))
+pygame.display.set_caption("street racer")
+clock = pygame.time.Clock()
+pygame.mixer.Sound("sounds\\background.wav").play()
+
+class Enemy(pygame.sprite.Sprite): 
+    def __init__(self): 
+        super().__init__()
+        self.image = pygame.image.load("images\\Enemy.png")
+        self.rect = self.image.get_rect()
+        self.rect.center = (random.randint(85, SCREEN_WIDTH - 85), 0)
+    
+    def move(self):
+        self.rect.move_ip(0, SPEED)
+        if self.rect.bottom > 600: 
+            self.rect.top = 0
+            self.rect.center = (random.randint(85, SCREEN_WIDTH -85), 0)
+    '''
+    def draw(self, surf):
+        surf.blit(self.image, self.rect) 
+    '''
+
+class Player(pygame.sprite.Sprite): 
+    def __init__(self): 
+        super().__init__()
+        self.image = pygame.image.load("images\\Player.png")
+        self.rect = self.image.get_rect()
+        self.rect.center = (160, 520)
+    
+    def move(self): 
+        pressed_keys = pygame.key.get_pressed() 
+        if self.rect.left > 50: 
+            if pressed_keys[pygame.K_LEFT]:
+                self.rect.move_ip(-5, 0)
+        if self.rect.right < SCREEN_WIDTH - 50: 
+            if pressed_keys[pygame.K_RIGHT]: 
+                self.rect.move_ip(5, 0)
+        if self.rect.top > 0: 
+            if pressed_keys[pygame.K_UP]:
+                self.rect.move_ip(0, -5)
+        if self.rect.bottom < SCREEN_HEIGHT: 
+            if pressed_keys[pygame.K_DOWN]: 
+                self.rect.move_ip(0, 5)
+    '''
+    def draw(self, surf): 
+        surf.blit(self.image, self.rect)
+    '''
+class Coin(pygame.sprite.Sprite): 
+    def __init__(self): 
+        super().__init__()
+        self.image = pygame.transform.scale(pygame.image.load("images\\Coin.png"), (2895//60.3, 2913//60.7))
+        self.rect = self.image.get_rect()
+        self.coin_weight = 1
+        self.rect.center = (random.randint(85, SCREEN_WIDTH -85), random.randint(10, SCREEN_HEIGHT - 10))
+    def spawn(self): 
+        self.coin_weight = random.randint(1, 3)
+        self.rect.center = (random.randint(85, SCREEN_WIDTH -85), random.randint(10, SCREEN_HEIGHT - 10))
+
+
+P1 = Player()
+E1 = Enemy()
+C1 = Coin()
+
+enemies = pygame.sprite.Group()
+all_sprites = pygame.sprite.Group()
+coins = pygame.sprite.Group()
+enemies.add(E1)
+coins.add(C1)
+all_sprites.add(P1)
+all_sprites.add(E1)
+all_sprites.add(C1)
+
+
+while True: 
+    for event in pygame.event.get():    
+        if event.type == pygame.QUIT: 
+            pygame.quit()
+            sys.exit() 
+
+    SURF.blit(background, (0, 0))
+    coin_num = font_small.render(str(COIN_NUM), True, (255, 255, 255))
+    SURF.blit(coin_num, (SCREEN_WIDTH - 35, 10))
+    for entity in all_sprites: 
+        SURF.blit(entity.image, entity.rect)
+        if not isinstance(entity, Coin): 
+            entity.move()
+
+    if pygame.sprite.spritecollideany(P1, enemies): 
+        pygame.mixer.Sound("sounds\\crash.wav").play()
+        time.sleep(0.5)
+
+        SURF.fill((255, 0, 0))
+        SURF.blit(game_over, (30, 250))
+
+        pygame.display.update()
+        for entity in all_sprites: 
+            entity.kill()
+        time.sleep(4)
+        pygame.quit()
+        sys.exit()
+
+    if pygame.sprite.spritecollideany(P1, coins): 
+        COIN_NUM += C1.coin_weight
+        COIN_REMEMBER += C1.coin_weight
+        if COIN_REMEMBER >= N: 
+            COIN_REMEMBER = 0
+            SPEED += 1
+        C1.spawn()
+    pygame.display.update()
+    clock.tick(60)
+    
+    
+
